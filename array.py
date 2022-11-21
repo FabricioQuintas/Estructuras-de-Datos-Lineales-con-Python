5ca7fe8df75d94627965cfd76951ec55d2c6fe93
Fabricio Quintas <fabri97n@gmail.com>
2022-11-21 15:52:51 -0300

    added comments to init, getitem and setitem functions

--- array.py
+++ array.py
@@ -1,7 +1,11 @@
 
 
-class Array:
+class Array: # Represent an array
     def __init__(self, capacity, fill_value=None):
+        '''
+        capacity(int) = static size of the array.
+        fill_value=(any, optional) = value at each position, None as default.
+        '''
         self.items = list()
         for i in range(capacity):
             self.items.append(fill_value)
@@ -16,7 +20,14 @@ class Array:
         return iter(self.items)
     
     def __getitem__(self, index):
+        '''
+        index(int) = index that we want to look at.
+        '''
         return self.items[index]
     
     def __setitem__(self, index, new_item):
+        '''
+        index(int) = index of number that we want to change.
+        new_item(any) = new value for the item in the array.
+        '''
         self.items[index] = new_item
